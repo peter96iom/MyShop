89098074a09ff8505b4704485faf22c9d22b4d39
Peter Frankwick <peter96iom@gmail.com>
2018-07-24 16:56:14 +0100

    Dependency injection

--- MyShop/MyShop.WebUI/Controllers/ProductCategoryController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryController.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.core.Models;
+﻿using MyShop.core;
+using MyShop.core.Models;
 using MyShop.DataAccess.InMemory;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,11 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductCategoryController : Controller
     {
-        InMemoryRepository<ProductCategory> context;
+        IRepository<ProductCategory> context;
 
-        public ProductCategoryController()
+        public ProductCategoryController(IRepository<ProductCategory> context)
         {
-            context = new InMemoryRepository<ProductCategory>();
+            this.context = context;
         }
         // GET: ProductManager
         public ActionResult Index()
---
+++ MyShop/MyShop.core/Contracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using MyShop.core.Models;
+
+namespace MyShop.core
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IQueryable Collection();
+        void Commit();
+        void Delete(string id);
+        T Find(string Id);
+        void insert(T t);
+        void update(T t);
+    }
+}
